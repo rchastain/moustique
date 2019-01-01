c020eebe88629e117eee743cb78711bb39f54d61
Roland Chastain <roland.chastain@gmx.fr>
2019-01-01 23:28:52 +0000

    Update matertest.pas
    New version of matertest.pas, including 711 mate problems solved by Mater. The problems were extracted from Franz Huber database.
    https://fhub.jimdo.com/

--- matertest.pas
+++ matertest.pas
@@ -1,8 +1,47 @@
 
 uses
-  Mater;
+  SysUtils, Mater;
 
+type
+  TMateProblem = record
+    depth: integer;
+    all: boolean;
+    epd: string;
+  end;
+
+const
+  PROBLEM: array[1..711] of TMateProblem = ({$I problems.inc});
+{
+  711 mate problems solved by Mater.
+  
+  The problems were extracted from Franz Huber database.
+  
+  https://fhub.jimdo.com/
+}
+
+procedure TrySolve(const aProblem: TMateProblem; const aIndex: integer);
+var
+  s: string;
 begin
-  WriteLn(SolveMate('b7/PP6/8/8/7K/6B1/6N1/4R1bk w KQkq -', 3, TRUE));
-  WriteLn(SolveMate('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1', 3, TRUE));
+  s := SolveMate(aProblem.epd, aProblem.depth, aProblem.all);
+  WriteLn(Format('%s problem=%0.3d depth=%d searchallmoves=%d solution=%s', [TimeToStr(Now), aIndex, aProblem.depth, byte(aProblem.all), s]));
+end;
+
+var
+  i: integer;
+  
+begin
+  for i := Low(PROBLEM) to High(PROBLEM) do with PROBLEM[i] do
+    if
+    (
+      (depth = 7) // Moves number.
+      and not all // Search mode (check only or all moves).
+    )
+    or
+    (
+      (depth = 3)
+      and all
+    )
+    then
+      TrySolve(PROBLEM[i], i);
 end.
